743877b656cd7a1ffd00368d1d7f8b27e47ec6ee
PanDeElote <verdisa17@gmail.com>
2025-06-09 13:50:44 -0600

    Notas extra

--- 02.Doing/semana2/app.mjs
+++ 02.Doing/semana2/app.mjs
@@ -3,15 +3,25 @@ import chalk from 'chalk';
 import saludar from './saludo.js';
 import figlet from 'figlet';
 
-inquirer.prompt([
+inquirer.prompt([ //Le pide al usuario que ingrese los datos. INQUIRER
   {
-    type: 'input',
-    name: 'nombre',
-    message: '¿Cuál es tu nombre?'
+    type: 'input', //Tipo de interacción
+    name: 'nombre', //Cómo se llama la cajita donde se va a guardar la info
+    message: '¿Cuál es tu nombre?' //El mensaje que le sale en consola al usuario
   },
   {
     type: 'input',
     name: 'edad',
+    validate: function (value) {
+    const parsed = parseInt(value, 10); //el 10 significa que es del sistema decimal
+    if (isNaN(parsed) || parsed < 0) {
+      return 'Por favor, ingresa un número entero válido.';
+    }else if(parsed > 150){
+      return 'No creo que seas Matusalen, ingresa una edad válida.'
+    }else{
+      return true;
+    }
+    },
     message: '¿Cuál es tu edad?'
   },
   {
@@ -19,17 +29,29 @@ inquirer.prompt([
     name: 'color',
     message: '¿Cuál es tu color favorito?',
     choices: ['red', 'blue', 'yellow', 'green', 'cyan']
+  },
+  {
+    type: 'select',
+    name: 'tarea',
+    message: 'hiciste la tarea de programación? ',
+    choices: ['si', 'no']
+  },
+  {
+    type: 'checkbox',
+    name: 'food',
+    message: 'Cuáles de las siguientes comidas te gustan? ',
+    choices: ['pizza', 'tacos', 'jicama', 'chapulines']
   }
-]).then(answers => {
-  let msjToShow = saludar(answers.nombre, answers.edad);
+]).then(answers => { //Todo lo anterior se va a guardar dentro de ANSWERS, y vas a interactuar con tal como array
+  let msjToShow = saludar(answers.nombre, answers.edad, answers.color, answers.tarea, answers.food); //Simplificando el nombre de la funcion que importé (SALUDAR)
 
-  figlet(answers.nombre, function (err, data) {
+  figlet(answers.nombre, function (err, data) { //Figlet para tipografía chida. DATA en este caso es nuestro answers.nombre
     if (err) {
       console.log("Algo salió mal...");
       return;
     }
-    console.log(chalk[answers.color](data));
+    console.log(chalk[answers.color](data)); //Si nada sale mal, primero aplicas el chalk. Anatomía: chalk[color a utilizar](en dónde se va a aplicar)
   });
 
-  console.log(chalk[answers.color](msjToShow));
+  console.log(chalk[answers.color](msjToShow)); //Ahora sí mostrando en consola mi función SALUDAR
 });
--- 02.Doing/semana2/saludo.js
+++ 02.Doing/semana2/saludo.js
@@ -1,11 +1,26 @@
-export default function saludar(nombre, edad) {
+import chalk from "chalk";
+
+export default function saludar(nombre, edad, tarea, food) {
+  console.log(`Hola ${nombre}! Qué bonito nombre, vamos a revisar tus datos.`);
+
+  let respuestaEdad = "";
   if (edad < 18) {
-    return "Eres menor de edad";
+    respuestaEdas = chalk['green']("Eres menor de edad,tas verdecito");
   } if (edad < 30) {
-    return "Estas en tus mejores años"
+    respuestaEdad = chalk['cyan']("Estas en tus mejores años");
   } if (edad < 60) {
-    return "Con experiencia y juventud"
+    respuestaEdad = chalk['magenta']("Con experiencia y juventud");
   } else {
-    return "Sabiduría acumulada"
+    respuestaEdad = chalk['blue']("Sabiduría acumulada");
   }
+
+  let respuestaTarea = "";
+  if(tarea === 'si'){
+    respuestaTarea = "Chambeador chambeador de la más alta calidad.";
+  }else{
+    respuestaTarea = "No se sabe la de chambear";
+  }
+
+  return `${respuestaEdad}\n ${respuestaTarea}\n ${food}`;
+  
 }
